9098bb454e71486222833ca7e576c2734bb281d6
Josevany do Amaral <josevanydoamaral@gmail.com>
2024-12-26 14:55:40 +0000

    Criação do das rotas get, post e delete

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 group = "com.app"
@@ -19,7 +20,9 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     testImplementation(libs.ktor.server.test.host)
--- src/main/kotlin/com/app/Routing.kt
+++ src/main/kotlin/com/app/Routing.kt
@@ -1,13 +1,51 @@
 package com.app
 
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Post(val id: Int, val title: String, val content: String)
+
+val posts = mutableListOf<Post>()
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("posts") {
+            call.respond(posts)
+        }
+        post("posts/add") {
+            val postsReceived = call.receive<List<Post>>()
+            // Adiciona cada post na lista de posts
+            posts.addAll(postsReceived)
+            // Responde com sucesso indicando o número de posts adicionados
+            call.respondText("Foram adicionados ${postsReceived.size} post(s): ${postsReceived.joinToString { it.title }}")
+        }
+
+        delete("/posts/delete/{id}") {
+            val postId = call.parameters["id"]?.toIntOrNull()
+
+            if (postId != null) {
+                val postToRemove = posts.find { it.id == postId }
+
+                if (postToRemove != null) {
+                    posts.remove(postToRemove) // Remove o post da lista
+                    call.respondText("Post com id $postId removido com sucesso.")
+                } else {
+                    call.respondText("Post com id $postId não encontrado.", status = HttpStatusCode.NotFound)
+                }
+            } else {
+                call.respondText("Id inválido.", status = HttpStatusCode.BadRequest)
+            }
         }
     }
 }
