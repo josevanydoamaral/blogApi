8a4d880b8b1d82d6347686e58d066756c01f3c15
Josevany do Amaral <josevanydoamaral@gmail.com>
2024-12-29 14:11:38 +0000

    Revert "Merge pull request #1 from josevanydoamaral/TiagoFirebase"
    This reverts commit 5d57a9aadcd244376f336477d0014c60e6eca127, reversing
    changes made to a5425415f3cc3cf0cbad27e66cc95e272b0d58cf.

--- src/main/kotlin/com/app/Application.kt
+++ src/main/kotlin/com/app/Application.kt
@@ -1,30 +1,18 @@
 package com.app
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import java.io.File
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
 
 fun main(args: Array<String>) {
-    EngineMain.main(args)
+    io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    // Inicializar o Firebase
-    initFirebase()
-
-    // Outras configurações
+    install(ContentNegotiation) {
+        json()
+    }
     configureRouting()
 }
 
-fun initFirebase() {
-    val serviceAccount = File("src/main/resources/firebase-service-account.json")
-    val options = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccount.inputStream()))
-        .setDatabaseUrl("https://blogapi-2bbdc.firebaseio.com")
-        .build()
 
-    FirebaseApp.initializeApp(options)
-}
